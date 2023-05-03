ec020c9a7faf21c56642509801597178d7202d50
Viktar <viktarbrutsis@gmail.com>
2023-05-03 09:30:52 +0300

    feat: add path

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,13 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
-const readableStream = fs.createReadStream('text222.txt', 'utf-8');
 let data = '';
+
+//путь к файлу
+let filePath = path.join(__dirname, 'text.txt');
+
+const readableStream = fs.createReadStream(filePath, 'utf-8');
 //частями (чанками) chunk 64KB
-//readableStream.on('data', chunk => console.log(chunk.length));
+readableStream.on('data', chunk => console.log(chunk.length));
 
 readableStream.on('data', chunk => data += chunk);
 
 //завершение потока - событие 'end'
+readableStream.on('data', () => console.log(data));
 readableStream.on('data', () => console.log('End', data.length));
 readableStream.on('error', error => console.log('Error', error.message));
+
+console.log(filePath);
