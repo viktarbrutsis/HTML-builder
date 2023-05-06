e7fe3e79a539f50b6b22a8b6ec37158164a062f5
Viktar Brutsis <viktarbrutsis@gmail.com>
2023-05-06 13:48:40 +0300

    feat: task3 done

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -8,11 +8,13 @@ const filePath = path.join(__dirname, 'secret-folder');
 
 const array = [];
 
+// Sometimes you want to retrieve the file name without the extension. You may use the built-in Node.js path module to do that. You can use the Path.parse method to parse a given file path into the individual parts. Then, retrieve the file name without the extension:
+
 fs.readdir(filePath, {withFileTypes: true }, (err, files) => {//проверяем папку 'secret-folder'
     for (let file of files) {
         fs.stat(path.join(filePath, file.name), (err, stats) => {
             if (stats.isFile()) {
-                stdout.write(`${file.name} -- ${path.extname(file.name)} -- ${stats.size}\n`)
+                stdout.write(`${path.parse(file.name).name} - ${path.extname(file.name).slice(1)} - ${stats.size / 1024}kb\n`)
                 // console.log(filePath);
                 // console.log(path.join(filePath, file.name));
                 // console.log(file.name);
